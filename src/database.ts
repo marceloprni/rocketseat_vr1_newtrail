d1d9f4e7c1ce307c3e123acf403dfb2a6e4c02fc
Marcelo Perini <67985323+marceloprni@users.noreply.github.com>
2024-04-11 22:24:14 -0300

    modificado para deply
    modificado para deploy

--- src/database.ts
+++ src/database.ts
@@ -6,11 +6,15 @@ if (!process.env.DATABASE_URL) {
 }
 
 export const config = {
-  client: 'sqlite3',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT, // 'sqlite3'
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
+  // connection: {
+  //  filename: env.DATABASE_URL,
+  // },
 }
 
 export const knex = setupKnex(config)
